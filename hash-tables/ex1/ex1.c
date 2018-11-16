f349e8a800b62d3a64860f08c22f6e496d386a55
James Phillips <bri79319@gmail.com>
2018-11-16 14:00:24 -0500

    implementing hash functionality for ex1 and ex2

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -8,15 +8,60 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
   HashTable *ht = create_hash_table(16);
 
   // YOUR CODE HERE
+  for (int i = 0; i < length; i++)
+  {
+    hash_table_insert(ht, weights[i], i);
+  }
 
+  for (int i = 0; i < length; i++)
+  {
+    int weight = weights[i];
+    int match = limit - weight;
+    int matchIndex = hash_table_retrieve(ht, match);
+
+    if (matchIndex != -1)
+    {
+      Answer *answer = malloc(sizeof(Answer));
+
+      int weightIndex = hash_table_retrieve(ht, weight);
+
+      if (weight == match)
+      {
+        for (int i = 0; i < length; i++)
+        {
+          if (weights[i] == weight && i != matchIndex)
+          {
+            weightIndex = i;
+          }
+        }
+      }
+
+      if (weightIndex > matchIndex)
+      {
+        answer->index_1 = weightIndex;
+        answer->index_2 = matchIndex;
+      }
+      else
+      {
+        answer->index_1 = matchIndex;
+        answer->index_2 = weightIndex;
+      }
+      destroy_hash_table(ht);
+      return answer;
+    }
+  }
+  destroy_hash_table(ht);
   return NULL;
 }
 
 void print_answer(Answer *answer)
 {
-  if (answer != NULL) {
+  if (answer != NULL)
+  {
     printf("%d %d\n", answer->index_1, answer->index_2);
-  } else {
+  }
+  else
+  {
     printf("NULL\n");
   }
 }
--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -10,13 +10,40 @@ char **reconstruct_trip(Ticket **tickets, int length)
   char **route = malloc(length * sizeof(char *));
 
   // YOUR CODE HERE
+  char *original;
 
+  for (int i = 0; i < length; i++)
+  {
+    if (strcmp(tickets[i]->source, "NONE") == 0)
+    {
+      original = strdup(tickets[i]->destination);
+      route[0] = original;
+    }
+    else
+    {
+      hash_table_insert(hash, tickets[i]->source, tickets[i]->destination);
+    }
+  }
+
+  int i = 1;
+
+  char *target_destination = hash_table_retrieve(hash, original);
+
+  while (strcmp(target_destination, "NONE") != 0)
+  {
+    route[i] = target_destination;
+    target_destination = hash_table_retrieve(hash, target_destination);
+    i++;
+  }
+  route[i] = target_destination;
+  destroy_hash_table(hash);
   return route;
 }
 
 void print_route(char **route, int length)
 {
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     printf("%s\n", route[i]);
   }
 }
